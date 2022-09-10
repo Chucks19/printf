c9ae38d7fe032ff947b686df35386e33c9268288
Cemordi2 <emordichukwuma@gmail.com>
2022-09-10 16:27:07 +0100

    int to binary converter

--- binary.c
+++ binary.c
@@ -3,6 +3,13 @@
 int binary(int n)
 {
        int count = 0;
+       if (n < 0)
+       {
+         n *= -1;
+         while (n >> 1 > 0)
+            count += binary(n >> 1);
+         count += _putchar((n % 2) + '0');
+       }
     if (n >> 1 > 0)
          count += binary (n >> 1);
       
