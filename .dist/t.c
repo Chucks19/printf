752e84c968a20cc410ef8300a761299fad6dbe20
Cemordi2 <emordichukwuma@gmail.com>
2022-09-11 15:36:09 +0100

    binaryfunctions that converts int to Octal and Hex

--- .dist/t.c
+++ .dist/t.c
@@ -3,9 +3,9 @@
 int main()
 {
        int A, B;
-       A = _printf("\n%d ok %o qww %x sjs 6\n", 2110, 13, 342);
+       A = _printf("\n%d ok %o qww %x sjs 6\n", 2110, 13, 351);
        _printf("\n%d\n", A);
-       B = printf("\n%d ok %o qww %x sjs 6\n", 2110, 13, 342);
+       B = printf("\n%d ok %o qww %x sjs 6\n", 2110, 13, 351);
        printf("\n%d\n", B);
 
        /**B = printf("\n%d ok qww sjs %b 6\n", 2110, 8);
