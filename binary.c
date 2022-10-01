399ebd7015a6a4ae6b1029600a6bdecf23b757d9
Cemordi2 <emordichukwuma@gmail.com>
2022-10-01 04:56:44 -0700

    bettyl style

--- binary.c
+++ binary.c
@@ -3,13 +3,13 @@
 int binary(int n)
 {
 	int count = 0;
+
 	if (n < 0)
 	{
 		n *= -2;
 	}
-	if (n >> 1 > 0)
-		count += binary (n >> 1);
-      
+	if(n >> 1 > 0)
+		count += binary (n >> 1);      
 	count += _putchar((n % 2) + '0');
 	return (count++);
 }
@@ -17,6 +17,7 @@ int binary(int n)
 int octa(int oct)
 {
 	int count = 0;
+
 	if (oct < 0)
 	{
 		oct *= -1;
@@ -24,13 +25,13 @@ int octa(int oct)
 	if ((oct / 8) > 0)
 		count += octa(oct / 8);
 	count += _putchar((oct % 8) + '0');
-	
 	return (count++);
 }
 
 int hexa(int hex)
 {
 	int count = 0;
+	
 	if (hex < 0)
 	{
 		hex *= -1;
@@ -38,13 +39,13 @@ int hexa(int hex)
 	if ((hex / 16) > 0)
 		count += hexa((hex / 16));
 	count += _putchar(prhex(hex % 16));
-
 	return(count);
 }
 
 int prhex(int H)
 {
 	int i = 1;
+
 	if (H >= 0 || H <= 9)
 		_putchar(H +'0');
 	else if (H == 10)
@@ -59,6 +60,5 @@ int prhex(int H)
 		_putchar('e');
 	else if (H == 15)
 		_putchar('f');
-
 	return (i);
 }
